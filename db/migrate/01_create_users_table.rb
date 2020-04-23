d88b54f74761254b2c05a442a2b0e344ec95f57d
N-Santiago <norbertosantiago82@gmail.com>
2020-04-23 11:34:55 -0800

    Done.

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -1,9 +1,13 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
+class CreateUsersTable < ActiveRecord::Migration[5.1]
 
     def change
         create_table :users do |t|
            t.string :name
            t.string :email
            t.string :password
-         end
-       end
+        end
+    end
+       
+end    
+     
+       
